1c1fbed7ecc3099210c326c49bc2a87a71c1ff7d
moraes7 <nicolasmoraes.dev@gmail.com>
2026-06-21 00:09:39 -0300

    fix: configurando rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,15 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    redirectTo: 'criancas',
+    pathMatch: 'full'
   },
   {
     path: 'criancas',
     loadComponent: () => import('./pages/criancas/criancas.page').then( m => m.CriancasPage)
   },
   {
-    path: 'carteira-vacinacao',
+    path: 'carteira-vacinacao/:id',
     loadComponent: () => import('./pages/carteira-vacinacao/carteira-vacinacao.page').then( m => m.CarteiraVacinacaoPage)
   },
 ];
