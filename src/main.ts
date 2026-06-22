80a4c7c14b84ef462872b33d05492683d7c18e9a
moraes7 <nicolasmoraes.dev@gmail.com>
2026-06-22 11:31:08 -0300

    feat: firebase configurado

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {enableProdMode} from '@angular/core';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {RouteReuseStrategy, provideRouter, withComponentInputBinding, withPreloading, PreloadAllModules} from '@angular/router';
+import {IonicRouteStrategy, provideIonicAngular} from '@ionic/angular/standalone';
 
-import { routes } from './app/app.routes';
-import { AppComponent } from './app/app.component';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+
+import {routes} from './app/app.routes';
+import {AppComponent} from './app/app.component';
+import {environment} from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes, withPreloading(PreloadAllModules), withComponentInputBinding()),
+    
+   provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+   provideFirestore(() => getFirestore())
   ],
 });
